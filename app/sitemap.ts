bc0be96ac2e8e3f757ec3f69e9f366ee99acb77c
jad-png <j.belassiria3993@uca.ac.ma>
2026-09-22 16:23:36 +0000

    allowign project domain to not be blocked by csp

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,7 +2,7 @@ import type { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return ["", "#histoire", "#menu", "safi", "#safi", "#adresse", "#contact"].map(
     (path) => ({
-      url: `https://amoreitaliano.ma/${path}`,
+      url: `https://amoreitalianosafi.ma/${path}`,
       lastModified: new Date(),
     }),
   );
